aa54f9d42880d77ce211563003e33cf11571740e
Ivan Danci <idanci@nebulent.com>
2013-04-06 12:57:08 +0300

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoreSquare::Application.config.secret_token = '816f6e6b39b74c2400b475686de24c10afb89b84c4a8116b5e86c07a4e45dcc8f0c866cd1df843eb40c08cf2e0734849c1f62fce295afdf342de20911febf522'
