c7303f70b4d83cf9796c432e9e4b55d730509a8a
Roman Roibu <roman.roibu@gmail.com>
2013-04-07 01:21:18 +0300

    Add check_in validation and functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,25 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   has_many :checkins
   has_many :squares, :through => :checkins
+
+  def check_in(square)
+    self.squares.push(square) if self.can_check_in?(square)
+  end
+
+  def check_out(square)
+    self.squares.delete(square)
+  end
+
+  def can_check_in?(square)
+    return false if self.squares.include? square
+    children = square.children
+    checkins = self.squares
+    unless children.empty?
+      children.each do |child|
+        return false unless checkins.include? child
+      end
+    end
+    return true
+  end
+
 end
