09bb1bfafd4dfb92a823f29bd82b81231e2e4160
Roman Roibu <roman.roibu@gmail.com>
2013-04-06 15:34:10 +0300

    Add registration and session controller to devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 LoreSquare::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :sessions => "users/sessions", :registrations => "users/registrations" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
