647abb580ff453078ac3366b33669a0a6b8f40ec
Roman Roibu <roman.roibu@gmail.com>
2013-04-07 08:41:00 +0300

    ......

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,11 @@ class PagesController < ApplicationController
   def test
   end
 
+  def index
+    @squares = Square.all
+    @names = @squares.map{|s| s.name}
+  end
+
   def acm
     respond_to do |format|
       format.json { render :json => File.read(Rails.root.to_s + "/app/views/pages/acm.json") }
